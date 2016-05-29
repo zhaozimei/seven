ebaafef220060cd72eb73187a7a25fb56d0dfa06
zhaozimei <1140173180@qq.com>
2016-05-29 21:53:22 +0800

    Update fibonacci_series.js

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function fibonacci_series(n) {
-
+  var arr = [];
+	arr[0] = 0;
+	arr[1] = 1; 
+	for(var i=2; i<n+1; i++){
+		var k=arr[i-2]+arr[i-1]
+		arr.push(k)
+	}
+return arr;
 }
 
 module.exports = fibonacci_series;
